6311c0e5285338ca40a21cd521b9db070311f740
rooreckless <rooreckless@gmail.com>
2023-07-27 17:39:09 +0900

    02 develop_env

--- apps/config.py
+++ apps/config.py
@@ -22,10 +22,7 @@ class LocalConfig(BaseConfig):
   mysql_endpoint=os.getenv("MYSQL_DEVENDPOINT")
   secret_key=os.getenv("SECRET_KEY")
   
-  print("--LocalConfig---")
-  
   SQLALCHEMY_DATABASE_URI = 'mariadb+pymysql://'+mysql_user+':'+mysql_password+'@'+mysql_endpoint+'/'+mysql_database
-  print("SQLALCHEMY_DATABASE_URI=",SQLALCHEMY_DATABASE_URI)
   SQLALCHEMY_TRACK_MODIFICATIONS=False
   SQLALCHEMY_ECHO = True
   SECRET_KEY=secret_key
@@ -36,18 +33,14 @@ class ProdConfig(BaseConfig):
   basedir=Path(__file__).parent.parent
   dotenv_path = os.path.join(basedir, '.env')
   load_dotenv(dotenv_path)
-  print("dotenv_path=",dotenv_path)
-  print("load_dotenv(dotenv_path)=",load_dotenv(dotenv_path))
   mysql_root_password=os.getenv("MYSQL_ROOT_PASSWORD")
   mysql_user=os.getenv("MYSQL_USER")
   mysql_password=os.getenv("MYSQL_PASSWORD")
   mysql_database=os.getenv("MYSQL_DATABASE")
   mysql_endpoint=os.getenv("MYSQL_ENDPOINT")
   secret_key=os.getenv("SECRET_KEY")
-  print("mysql_endpoint=",mysql_endpoint)
-  print("-=-=-ProdConfig-=-=-")
+
   SQLALCHEMY_DATABASE_URI = 'mariadb+pymysql://'+mysql_user+':'+mysql_password+'@'+mysql_endpoint+'/'+mysql_database
-  # SQLALCHEMY_DATABASE_URI=f"sqlite:///{basedir/'testing.sqlite'}"
   SQLALCHEMY_TRACK_MODIFICATIONS=False
   SQLALCHEMY_ECHO = True
   SECRET_KEY=secret_key
