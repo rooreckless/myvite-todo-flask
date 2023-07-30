b9a7487de811ac2949f83562eae2b1b202662206
rooreckless <rooreckless@gmail.com>
2023-07-30 18:50:38 +0900

    03 dev_env2

--- apps/app.py
+++ apps/app.py
@@ -1,6 +1,6 @@
 #app.py
 #config切り替えにはconfig.pyのconfig辞書が使えればいい。
-from apps.config import config
+# from apps.config import config
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -15,8 +15,17 @@ from apps.back_app import views
 def create_app(config_key):
   app=Flask(__name__)
   app.register_blueprint(views.back_app,url_prefix="/back_app")
-  print("create_app----config_key=",config_key)
-  app.config.from_object(config[config_key])
+
+  # app.config.from_object(config[config_key])
+  if config_key=="product":
+    print("create_app----config_key=",config_key)
+    app.config.from_pyfile("../.eenv.py")
+  elif config_key=="local":
+    print("create_app----config_key=",config_key)
+    app.config.from_pyfile("../dev.env.py")
+  else:
+    print("create_app----config_key=",config_key)
+    app.config.from_pyfile("../dev.env.py")
   #SQLAlchemyとアプリを連携する初期化
   db.init_app(app)
   # #Migrateインスタンス作成 appとdbを入れて連携させる
